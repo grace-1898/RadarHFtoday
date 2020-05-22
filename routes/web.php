9ec25e3d9c2505704f4cf5463c9557e33599e5e4
Grace Agave <graceagave@gmail.com>
2020-05-23 01:28:08 +0700

    authentication

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,11 @@ Route::get('/Tampilpanah/gettampilpanah/{jam}/{hari}', 'TampilpanahController@ge
  
  
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
